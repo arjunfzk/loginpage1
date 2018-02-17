5907608eeaa1bb7d2048410992f0df626c99ea6d
arjunfzk <32428467+arjunfzk@users.noreply.github.com>
2018-02-17 15:15:56 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('test.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    import requests
+    import json
+    text = request.form['textt']
+    text2 = request.form['text2']
+    if text=="admin" and text2=="password":
+        return "<h1>WELL DONE,now show your code and get your name registered</h1>"
+    else:
+    	return "NONO"
+if __name__ == '__main__':
+    app.run(debug=True)
